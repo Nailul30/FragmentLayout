bd5dd133e4307105dadba26bd3eaae8c10abc081
Nailul <hidayat.nailul11@gmail.com>
2022-04-20 20:33:53 +0800

    add

--- app/src/androidTest/java/comf55119084/example/fragmentlayout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/comf55119084/example/fragmentlayout/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("comf55119035.example.fragmentlayout", appContext.getPackageName());
+        assertEquals("comf55119084.example.fragmentlayout", appContext.getPackageName());
     }
 }
